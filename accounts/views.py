b57efef700a02c7abd5d056e9408e2413f933557
VovaGul <vovagul1999@mail.ru>
2020-04-05 17:42:05 +0500

    update profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import CreateView
-# from accounts.models import Profile
-from django.contrib.auth.models import User
+from accounts.models import Profile
+# from django.contrib.auth.models import User
 
 
 class ProfileView(CreateView):
-    model = User
-    fields = ('first_name', 'last_name', 'email')
+    model = Profile
+    fields = ('birth_date',)
     template_name = 'accounts/templates/profile/profile_form.html'
