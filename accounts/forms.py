2fb7db19d659e33ca9ef7f535a143cc09228af71
VovaGul <vovagul1999@mail.ru>
2020-04-05 15:53:19 +0500

    link profile to database

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,16 +1,15 @@
-from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from accounts.models import Profile
+# from django import forms
+# from accounts.models import Profile
+# from django.contrib.auth.models import User
 
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('name', 'email', 'job_title', 'bio')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Save person'))
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'email')
+#
+#
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ('url', 'location', 'company')
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,9 @@
-from django.views.generic import CreateView, UpdateView
-from accounts.models import Profile
-from accounts.forms import ProfileForm
+from django.views.generic import CreateView
+# from accounts.models import Profile
+from django.contrib.auth.models import User
 
 
 class ProfileView(CreateView):
-    model = Profile
-    fields = ('name', 'email', 'job_title', 'bio')
+    model = User
+    fields = ('first_name', 'last_name', 'email')
     template_name = 'accounts/templates/profile/profile_form.html'
-
-
-# class PersonUpdateView(UpdateView):
-#     model = Profile
-#     form_class = ProfileForm
-#     template_name = 'accounts/templates/profile/profile_update_form.html'
